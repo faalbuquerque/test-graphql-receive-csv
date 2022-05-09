ec36d2e8ce9885223e8fc922be96d9fcf68f6716
Fabiane Albuquerque <bii.nett@gmail.com>
2022-05-09 15:46:57 -0300

    Rails new and install graphql

--- app/graphql/types/upload_type.rb
+++ app/graphql/types/upload_type.rb
@@ -1,6 +1,5 @@
 class Types::UploadType < Types::BaseInputObject
   graphql_name 'Upload'
-  description 'Argumentos necessarios para criar um autor'
 
   argument :filename, String, required: false
   argument :mimetype, String, required: false
